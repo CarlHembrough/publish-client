8ecdf342a2c87b80bf18386e7cdc5b5140ae82de
carlhuk <carlhuk@gmail.com>
2016-01-25 09:35:53 +0000

    Initial commit. Added test data and build configuration.

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        // read the train url from args
+
+        // read the content from classpath resources
+
+        // measure the time taken for each call
+
+        // start transaction
+
+        // commit each file in the resources - add the test content path to each file
+
+        // before the commit, ensure the transaction structure is as expected with the files in the test directory.
+        // commit the transaction
+
+        System.out.println("hello");
+    }
+
+}
